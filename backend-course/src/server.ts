b862ec95cfc8ba7d3476f34d380630d47c554554
septume <duchene.nico.som@gmail.com>
2022-11-02 17:01:36 +0100

    router backend

--- backend-course/src/server.ts
+++ backend-course/src/server.ts
@@ -1,9 +1,7 @@
-import { sample_foods, sample_tags, sample_users } from "./data";
-
 import cors from "cors";
 import express from "express";
-// npm install jsonwebtoken
-import jwt from "jsonwebtoken";
+import foodRouter from './routers/food.router';
+import userRouter from './routers/user.router';
 
 const app = express();
 app.use(express.json());
@@ -12,57 +10,9 @@ app.use(cors({
     origin:["http://localhost:4200"]
 }));
 
-app.get("/api/foods", (req, res) => {
-    res.send(sample_foods);
-})
+app.use("/api/foods", foodRouter);
+app.use("/api/users", userRouter);
 
-app.get("/api/foods/search/:searchTerm", (req, res) => {
-  const searchTerm = req.params.searchTerm;
-  const foods = sample_foods
-    .filter(food => food.nom.toLowerCase()
-    .includes(searchTerm.toLowerCase()))
-  res.send(foods);
-})
-
-app.get("/api/foods/tags", (req, res) => {
-  res.send(sample_tags);
-})
-
-app.get("/api/foods/tag/:tagName", (req,res) => {
-  const tagName = req.params.tagName;
-  const foods = sample_foods
-  .filter(food => food.tags?.includes(tagName));
-  res.send(foods);
-})
-
-app.get("/api/foods/:foodId", (req, res) => {
-  const foodId = req.params.foodId;
-  const food = sample_foods.find(food => food.id == foodId);
-  res.send(food);
-})
-
-app.post("/api/users/login", (req, res) => {
-  const {email, password} = req.body;
-  const user = sample_users.find(user => user.email === email &&
-    user.password === password)
-
-    if(user){
-      res.send(generateTokenResponse(user));
-    }else{
-      res.status(400).send("User name or password is not valid!");
-    }
-})
-
-const generateTokenResponse = (user:any) => {
- const token = jwt.sign({
-  email:user.email, isAdmin:user.isAdmin
- }, "SomeRandomText", {
-  expiresIn:"30d"
- });
-
- user.token = token;
- return user;
-}
 
 const port = 5000;
 app.listen(port, () => {
